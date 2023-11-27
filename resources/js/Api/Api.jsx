cf1bcc2204e9824e47c444aa7255ccf18fcc47be
Priya Bagus Amanullah <priyabagus.a29@gmail.com>
2023-11-27 15:58:09 +0700

    Update Api.jsx

--- resources/js/Api/Api.jsx
+++ resources/js/Api/Api.jsx
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export const getAllFood = async () => {
     try {
-        const response = await axios.get(`http://localhost:8000/api/products`);
+        // const response = await axios.get(`http://localhost:8000/api/products`);
+        const response = await axios.get(
+            `https://larareact.domcloud.dev/api/products`
+        );
         return response.data;
     } catch (err) {
         console.log(err.response);
@@ -11,7 +14,10 @@ export const getAllFood = async () => {
 
 export const getAllBeverages = async () => {
     try {
-        const response = await axios.get(`http://localhost:8000/api/beverages`);
+        // const response = await axios.get(`http://localhost:8000/api/beverages`);
+        const response = await axios.get(
+            `https://larareact.domcloud.dev/api/beverages`
+        );
         return response.data;
     } catch (err) {
         console.log(err.response);
